2ba03bf86ca936b7de2246efee316fc443e929f4
UnfazedAy <ayomidesoniyi@gmail.com>
2024-10-30 07:27:55 +0100

    Corrected the issue with the mongo_uri key setup

--- config/keys.js
+++ config/keys.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config({ path: '.env' });
 
 const keys = {
-  MONGO_URI: process.env.MONGO_URI === 'development' ? process.env.MONGO_URI_DEV : process.env.MONGO_URI_PROD,
+  MONGO_URI: process.env.NODE_ENV === 'development' ? process.env.MONGO_URI_DEV : process.env.MONGO_URI_PROD,
   PORT: process.env.PORT || 3000,
   HOST: process.env.HOST || 'localhost',
   NODE_ENV: process.env.NODE_ENV || 'development',
