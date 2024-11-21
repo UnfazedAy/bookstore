20ebf33e9eff4fbfd6bd7fe998d46acbe3105e43
UnfazedAy <ayomidesoniyi@gmail.com>
2024-11-21 11:59:23 +0100

    Making it serverless so I can make use of vercel

---
+++ local.js
@@ -0,0 +1,16 @@
+import app from './server.js';
+import keys from './config/keys.js';
+import logger from './config/logger.js';
+
+const { PORT, HOST } = keys;
+
+// Start the server locally
+const server = app.listen(PORT, () => {
+  logger.info(`Server running locally at http://${HOST}:${PORT}`.cyan.bold);
+});
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err) => {
+  logger.error(`Error: ${err.message}`.red);
+  server.close(() => process.exit(1));
+});
